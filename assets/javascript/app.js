57f7d5044c28075f9cab2932f176fd79943a5762
Alexander Larsen <spacemaneuver89gmail.com>
2018-08-29 16:25:54 -0600

    submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $('#start').on('click', function () {
 	game.start();
 })
 
-$(document).on('click','#end',function(){
+$(document).on('click', '#end', function () {
 	game.done();
 })
 
@@ -23,11 +23,11 @@ var questions = [{
 }, {
 	question: "The ships primary super computer is named?",
 	answers: ["MUTHUR", "CASPAR", "FATHUR", "DAVID"],
-	correctAnswer: "MUTHER"
+	correctAnswer: "MUTHUR"
 }, {
 	question: "What crewmember is revealed to be a synthetic android unbeknownst to the crew?",
 	answers: ["Ripley", "Bishop", "Dallas", "Ash"],
-	correctAnswer: "first test"
+	correctAnswer: "Ash"
 }, {
 	question: "Ripley escapes on a lifeboat named _____ at the end of the film.",
 	answers: ["Narcissus", "Patna", "Marlow", "Verloc"],
@@ -38,90 +38,93 @@ var questions = [{
 }];
 //game logic
 var game = {
-		correct: 0,
-		incorrect: 0,
-		counter: 20,
-		countdown: function(){
-			game.counter--;
-			$('#counter').html(game.counter);
-			if (game.counter<=0){
-				console.log("Time's up");
-				game.done();
+	correct: 0,
+	incorrect: 0,
+	counter: 120,
+	countdown: function () {
+		game.counter--;
+		$('#counter').html(game.counter);
+		if (game.counter <= 0) {
+			console.log("Time's up");
+			game.done();
+		}
+	},
+
+	//starts game when start putton is clicked. This reveals the questions and answers on the HTML and starts the timer.
+	start: function () {
+		timer = setInterval(game.countdown, 1000);
+		$('#subwrapper').prepend('<h2>Time Remaining: <span id= "counter">120</span> Seconds</h2>');
+		$('#start').on('click', function () {
+			$('#start').remove();
+			for (var i = 0; i < questions.length; i++) {
+				$('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
+				for (var j = 0; j < questions[i].answers.length; j++) {
+					$('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+				}
 			}
-		},
+			$('#subwrapper').append('<br><button id="end">DONE</button>');
 
-		//starts game when start putton is clicked. This reveals the questions and answers on the HTML and starts the timer.
-		start: function (){
-				timer = setInterval(game.countdown,1000);
-				$('#subwrapper').prepend('<h2>Time Remaining: <span id= "counter">120</span> Seconds</h2>');
-				$('#start').on('click', function () {
-						$('#start').remove();
-						for (var i=0;i<questions.length;i++){
-							$('#subwrapper').append('<h2>' + questions[i].question+ '</h2>');
-							for (var j = 0; j < questions[i].answers.length; j++) {
-								$('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
-								}
-						}
-						$('#subwrapper').append('<br><button id="end">DONE</button>');
+		})
+	},
+	//Each of these functions are unique to each Q and A in the questions array and will check if each answer is correct or incorrect.
+	done: function () {
+		$.each($("input[name='question-0']:checked"), function () {
+			if ($(this).val() == questions[0].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
+				//clicked true/false add conditional
+			}
+		});
+		$.each($("input[name='question-1']:checked"), function () {
+			if ($(this).val() == questions[1].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($("input[name='question-2']:checked"), function () {
+			if ($(this).val() == questions[2].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($("input[name='question-3']:checked"), function () {
+			if ($(this).val() == questions[3].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
+		$.each($("input[name='question-4']:checked"), function () {
+			if ($(this).val() == questions[4].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
 
-				}
-				)},
-//Each of these functions are unique to each Q and A in the questions array and will check if each answer is correct or incorrect.
-				done: function(){
-					$.each($("input[name='question-0']:checked"),function(){
-						if($(this).val()== questions[0].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
-					$.each($("input[name='question-1']:checked"),function(){
-						if($(this).val()== questions[1].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
-					$.each($("input[name='question-2']:checked"),function(){
-						if($(this).val()== questions[2].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
-					$.each($("input[name='question-3']:checked"),function(){
-						if($(this).val()== questions[3].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
-					$.each($("input[name='question-4']:checked"),function(){
-						if($(this).val()== questions[4].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
-					$.each($("input[name='question-5']:checked"),function(){
-						if($(this).val()== questions[5].correctAnswer){
-							game.correct++;
-						} else {
-							game.incorrect++;
-						}
-					});
+			}
+		});
+		$.each($("input[name='question-5']:checked"), function () {
+			if ($(this).val() == questions[5].correctAnswer) {
+				game.correct++;
+			} else {
+				game.incorrect++;
+			}
+		});
 
-					this.result();
-				},
+		this.result();
+	},
 
-				result: function(){
-					clearInterval(timer);
-					$('#subwrapper h2').remove();
+	result: function () {
+		clearInterval(timer);
+		$('#subwrapper h2').remove();
 
-					$('#subwrapper').html("<h2>Complete!</h2>");
+		$('#subwrapper').html("<h2>Complete!</h2>");
 
-					$('#subwrapper').append("<h3>Correct Answers: "+this.correct+ "</h3>");
-					$('#subwrapper').append("<h3>incorrect Answers: "+this.incorrect+ "</h3>");
-					$('#subwrapper').append("<h3>Unanswered: "(questions.length-(this.incorrect+this.correct))+"</h3>");
-				}
+		$('#subwrapper').append("<h3>Correct Answers: " + this.correct + "</h3>");
+		$('#subwrapper').append("<h3>incorrect Answers: " + this.incorrect + "</h3>");
+		$('#subwrapper').append("<h3>Unanswered: " (questions.length - (this.incorrect + this.correct)) + "</h3>");
+		
+	}
 }
